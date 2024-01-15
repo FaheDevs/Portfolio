d821efaec307d81b732a2393b2f58f76262d11d8
fahed <zidanifahd@gmail.com>
2024-01-15 20:15:30 +0100

    setup/tailwind css header

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'backgroundColor': 'gray-50',
+        'backgroundColorOne': '#fbe2e3',
+        'backgroundColorTwo': '#dbd7fb',
+      },
     },
   },
   plugins: [],
